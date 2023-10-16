9cd4029aeb0c62607983173b8de600f542176b1b
Abh2004 <asm2004@kgpian.iitkgp.ac.in>
2023-10-16 17:28:57 +0530

    env included

--- News/views.py
+++ News/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
-API_KEY = 'API KEY HERE I HAD TOLD IN THE VIDEO HOW TO GET ONE'
+
 # Create your views here.
+API_KEY = '4fce03da4e834c67a277777ae74631f5'
+import requests
+from django.http import HttpResponse
+from django.views.decorators.cache import never_cache  # Import never_cache decorator
+
+
+@never_cache  # Decorate your view function with never_cache
 def index(request):
     url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}'
     data = requests.get(url)
     response = data.json()
     articles = response['articles']
-    context = {'articles':articles}
-    return render(request,'index.html',context)
+    context = {'articles': articles}
+
+    response = render(request, 'index.html', context)
+    
+    # Set cache control headers to prevent caching
+    response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response['Pragma'] = 'no-cache'
+    response['Expires'] = '0'
+
+    return response
+
+
+
 
 def category(request,name):
-    url = f'https://newsapi.org/v2/top-headlines?category={name}&apiKey={API_KEY}'
+    # url = f'https://newsapi.org/v2/top-headlines?category={name}&apiKey={API_KEY}'
+    url = f'https://newsapi.org/v2/top-headlines?country=in&category={name}&apiKey={API_KEY}'
     data = requests.get(url)
     response = data.json()
     articles = response['articles']
@@ -25,3 +44,5 @@ def search(request):
     articles = response['articles']
     context = {'articles':articles,'search':search_term}
     return render(request,'search.html',context)
+
+
